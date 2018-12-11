f5b9b6979851bcb662abdcd010f5907718716260
Zvli <1203723071@qq.com>
2018-12-11 15:05:00 +0800

    project init commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class Test {
+
+    public void number(int n){
+
+        HashSet<Integer> redAarray = new HashSet<Integer>();
+        Random random = new Random();
+
+        for (;;) {
+            redAarray.add(random.nextInt(33)+1);
+            if(redAarray.size()==6) break;
+        }
+        List<Integer> redList=new ArrayList<Integer>(redAarray);
+        Collections.sort(redList);
+        //System.out.print(redAarray);
+        for (Integer i:redList){
+            System.out.print(i+" ");
+        }
+        System.out.println(random.nextInt(16)+1);
+    }
+
     public static void main(String[] args) {
-        B b = new B();
+        Test test=new Test();
+        test.number(0);
     }
 }
